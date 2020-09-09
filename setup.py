af67c4dda2d7fb7679fd7e30d03a514fabe95581
Ajay2810 <lingayatajay2810@gmail.com>
2020-09-09 12:46:12 +0530

    Initial commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = 'covid19-tracker',
-    version = '0.0.7',
+    version = '0.0.8',
     author="Ajay Lingayat",
     author_email="lingayatajay2810@gmail.com",
     description = "This module helps to track Covid19.",
